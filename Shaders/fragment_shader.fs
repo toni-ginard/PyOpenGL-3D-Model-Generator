8992c0cf827ed4505791dbf9c9df60b047afe976
Toni Ginard <toniginard94@gmail.com>
2019-03-17 16:47:32 +0100

    Recrear shaders inicials (color i distribució de la posició)

--- Shaders/fragment_shader.fs
+++ Shaders/fragment_shader.fs
@@ -1,25 +1,8 @@
 #version 330
-in vec2 newTexture;
-in vec3 fragNormal;
-in vec3 fragPos;
 
 out vec4 outColor;
 
 void main()
 {
-    vec3 lightColor = vec3(1.0f, 1.0f, 1.0f);
-    vec3 lightPos = vec3(1.2, 1.0, 2.0);
-    vec3 objectColor = vec3(1.0f, 0.5f, 0.31f);
-
-    vec3 ambient = lightColor * 0.1f; // ambientStrength
-
-    vec3 norm = normalize(fragNormal);
-    vec3 lightDir = normalize(lightPos - fragPos);
-
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-
-    vec3 result = (ambient + diffuse) * objectColor;
-
-    outColor = vec4(result, 1.0);
+    outColor = vec4(0.8, 0.2, 0.0, 1.0);
 }
--- Shaders/vertex_shader.vs
+++ Shaders/vertex_shader.vs
@@ -1,19 +1,12 @@
 #version 330
 
 layout (location = 0) in vec3 position;
-layout (location = 1) in vec3 vertNormal;
 
 uniform mat4 proj;
 uniform mat4 view;
 uniform mat4 model;
-uniform mat4 light;
-
-out vec3 fragNormal;
-out vec3 fragPos;
 
 void main()
 {
     gl_Position = proj * view * model * vec4(position, 1.0);
-    fragPos = vec3(model * vec4(position, 1.0));
-    fragNormal = vertNormal;
 }
