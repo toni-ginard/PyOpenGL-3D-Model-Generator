5f1ccfb50105dd7260d10725b9ecff72fc793df7
Toni Ginard <toniginard94@gmail.com>
2019-03-19 11:15:27 +0100

    Intent 3: ja es pot representar més d'un model

--- main.py
+++ main.py
@@ -41,11 +41,12 @@ def main():
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 12, ctypes.c_void_p(0))
     glEnableVertexAttribArray(0)
 
-    """
+    # PIRAMIDE
     pir = Piramide()
     pir_vao = glGenVertexArrays(1)
     glBindVertexArray(pir_vao)
-    pir_shader = ShaderLoader.compile_shader("Figures/Piramide/vertex_shader.vs", "Figures/Piramide/fragment_shader.fs")
+    pir_shader = ShaderLoader.compile_shader("Figures/Piramide/vertex_piramide.vs",
+                                             "Figures/Piramide/fragment_piramide.fs")
     pir_vbo = glGenBuffers(1)
     glBindBuffer(GL_ARRAY_BUFFER, pir_vbo)
     glBufferData(GL_ARRAY_BUFFER, pir.vertexs.nbytes, pir.vertexs, GL_STATIC_DRAW)
@@ -55,7 +56,7 @@ def main():
     glGetAttribLocation(pir_shader, "position")
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 12, ctypes.c_void_p(0))
     glEnableVertexAttribArray(0)
-    """
+
     proj = pyrr.matrix44.create_perspective_projection_matrix(45.0, width / height, 0.1, 100.0)
 
     view = pyrr.matrix44.create_from_translation(pyrr.Vector3([0.0, 0.0, -4.0]))
@@ -65,13 +66,13 @@ def main():
     proj_loc = glGetUniformLocation(cub_shader, "proj")
     glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
     glUniformMatrix4fv(proj_loc, 1, GL_FALSE, proj)
-    """
+
     glUseProgram(pir_shader)
     view_loc_pir = glGetUniformLocation(pir_shader, "view")
     proj_loc_pir = glGetUniformLocation(pir_shader, "proj")
     glUniformMatrix4fv(view_loc_pir, 1, GL_FALSE, view)
     glUniformMatrix4fv(proj_loc_pir, 1, GL_FALSE, proj)
-    """
+
     cube_positions = [(2.0, 5.0, -15.0), (-1.5, -1.2, -2.5), (1.0, -0.0, -4.0)]
 
     glEnable(GL_DEPTH_TEST)  # profunditat
@@ -82,17 +83,18 @@ def main():
         # for i in range(len(cube_positions)):
             # Cub.dibuixar_cub(cube_positions[i], cub_shader, cub.indexs)
         glBindVertexArray(cub_vao)
-        model = pyrr.matrix44.create_from_translation(cube_positions[0])
+        model = pyrr.matrix44.create_from_translation(cube_positions[1])
         model_loc = glGetUniformLocation(cub_shader, "model")
         glUniformMatrix4fv(model_loc, 1, GL_FALSE, model)
         glDrawElements(GL_TRIANGLES, cub.indexs.size, GL_UNSIGNED_INT, None)
         glBindVertexArray(0)
-        """
-        model_pir = pyrr.matrix44.create_from_translation(cube_positions[1])
+
+        glBindVertexArray(pir_vao)
+        model_pir = pyrr.matrix44.create_from_translation(cube_positions[0])
         model_loc_pir = glGetUniformLocation(pir_shader, "model")
         glUniformMatrix4fv(model_loc_pir, 1, GL_FALSE, model_pir)
         glDrawElements(GL_TRIANGLES, pir.indexs.size, GL_UNSIGNED_INT, None)
-        """
+        glBindVertexArray(0)
 
         glfw.swap_buffers(window)
 
